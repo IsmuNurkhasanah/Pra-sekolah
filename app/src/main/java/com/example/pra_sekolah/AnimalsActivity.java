86b7f6ed39bbd174b5625a0afc5cee7c4c02152f
IsmuNurkhasanah <ismu.nk05@gmail.com>
2025-06-22 19:24:46 +0700

    update color

--- app/src/main/java/com/example/pra_sekolah/AnimalsActivity.java
+++ app/src/main/java/com/example/pra_sekolah/AnimalsActivity.java
@@ -58,182 +58,182 @@ public class AnimalsActivity extends AppCompatActivity {
         imageA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Rusa?", R.drawable.antelope);
+                bukalamanHewan("Hewan Rusa itu apa?", R.drawable.antelope);
             }
         });
         ImageView imageB = findViewById(R.id.image_b);
         imageB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Beruang?", R.drawable.bear);
+                bukalamanHewan("Hewan Beruang itu apa?", R.drawable.bear);
             }
         });
         ImageView imageC = findViewById(R.id.image_c);
         imageC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kucing?", R.drawable.cat);
+                bukalamanHewan("Hewan Kucing itu apa?", R.drawable.cat);
             }
         });
         ImageView imageD = findViewById(R.id.image_d);
         imageD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Anjing?", R.drawable.dog);
+                bukalamanHewan("Hewan Anjing itu apa?", R.drawable.dog);
             }
         });
         ImageView imageE = findViewById(R.id.image_e);
         imageE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Gajah?", R.drawable.elephant);
+                bukalamanHewan("Hewan Gajah itu apa?", R.drawable.elephant);
             }
         });
         ImageView imageF = findViewById(R.id.image_f);
         imageF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Rubah?", R.drawable.fox);
+                bukalamanHewan("Hewan Rubah itu apa?", R.drawable.fox);
             }
         });
         ImageView imageG = findViewById(R.id.image_g);
         imageG.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Jerapah?", R.drawable.giraffe);
+                bukalamanHewan("Hewan Jerapah itu apa?", R.drawable.giraffe);
             }
         });
         ImageView imageH = findViewById(R.id.image_h);
         imageH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kuda?", R.drawable.horse);
+                bukalamanHewan("Hewan Kuda itu apa?", R.drawable.horse);
             }
         });
         ImageView imageI = findViewById(R.id.image_i);
         imageI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Iguana?", R.drawable.iguana);
+                bukalamanHewan("Hewan Iguana itu apa?", R.drawable.iguana);
             }
         });
         ImageView imageJ = findViewById(R.id.image_j);
         imageJ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Jaguar?", R.drawable.jaguar);
+                bukalamanHewan("Hewan Jaguar itu apa?", R.drawable.jaguar);
             }
         });
         ImageView imageK = findViewById(R.id.image_k);
         imageK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kanguru?", R.drawable.kangaroo);
+                bukalamanHewan("Hewan Kanguru itu apa?", R.drawable.kangaroo);
             }
         });
         ImageView imageL = findViewById(R.id.image_l);
         imageL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Singa?", R.drawable.lion);
+                bukalamanHewan("Hewan Singa itu apa?", R.drawable.lion);
             }
         });
         ImageView imageM = findViewById(R.id.image_m);
         imageM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Monyet?", R.drawable.monkey);
+                bukalamanHewan("Hewan Monyet itu apa?", R.drawable.monkey);
             }
         });
         ImageView imageN = findViewById(R.id.image_n);
         imageN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kadal?", R.drawable.newt);
+                bukalamanHewan("Hewan Kadal itu apa?", R.drawable.newt);
             }
         });
         ImageView imageO = findViewById(R.id.image_o);
         imageO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Burung Hantu?", R.drawable.owl);
+                bukalamanHewan("Hewan Burung Hantu itu apa?", R.drawable.owl);
             }
         });
         ImageView imageP = findViewById(R.id.image_p);
         imageP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Pinguin?", R.drawable.penguin);
+                bukalamanHewan("Hewan Pinguin itu apa?", R.drawable.penguin);
             }
         });
         ImageView imageQ = findViewById(R.id.image_q);
         imageQ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Burung Puyuh?", R.drawable.quail);
+                bukalamanHewan("Hewan Burung Puyuh itu apa?", R.drawable.quail);
             }
         });
         ImageView imageR = findViewById(R.id.image_r);
         imageR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kelinci?", R.drawable.rabbit);
+                bukalamanHewan("Hewan Kelinci itu apa?", R.drawable.rabbit);
             }
         });
         ImageView imageS = findViewById(R.id.image_s);
         imageS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Ular?", R.drawable.snake);
+                bukalamanHewan("Hewan Ular itu apa?", R.drawable.snake);
             }
         });
         ImageView imageT = findViewById(R.id.image_t);
         imageT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Harimau?", R.drawable.tiger);
+                bukalamanHewan("Hewan Harimau itu apa?", R.drawable.tiger);
             }
         });
         ImageView imageU = findViewById(R.id.image_u);
         imageU.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Sapi?", R.drawable.urial);
+                bukalamanHewan("Hewan Sapi itu apa?", R.drawable.urial);
             }
         });
         ImageView imageV = findViewById(R.id.image_v);
         imageV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Burung Nasar?", R.drawable.vulture);
+                bukalamanHewan("Hewan Burung Nasar itu apa?", R.drawable.vulture);
             }
         });
         ImageView imageW = findViewById(R.id.image_w);
         imageW.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Serigala?", R.drawable.wolf);
+                bukalamanHewan("Hewan Serigala itu apa?", R.drawable.wolf);
             }
         });
         ImageView imageX = findViewById(R.id.image_x);
         imageX.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Meerkat?", R.drawable.xerus);
+                bukalamanHewan("Hewan Meerkat itu apa?", R.drawable.xerus);
             }
         });
         ImageView imageY = findViewById(R.id.image_y);
         imageY.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Kerbau?", R.drawable.yak);
+                bukalamanHewan("Hewan Kerbau itu apa?", R.drawable.yak);
             }
         });
         ImageView imageZ = findViewById(R.id.image_z);
         imageZ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bukalamanHewan("Apa itu Hewan Zebra?", R.drawable.zebra);
+                bukalamanHewan("Hewan Zebra itu apa?", R.drawable.zebra);
             }
         });
     }
